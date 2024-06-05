b543248b11ae726ef9c4177af9adecf5db07369f
Bárbara Luciano Araújo <90732683+babi2707@users.noreply.github.com>
2024-06-05 11:53:45 -0300

    reduzir subimagens para 20 de cada classe

--- PREPROCESSAMENTO/prepross.py
+++ PREPROCESSAMENTO/prepross.py
@@ -9,8 +9,15 @@ classifications = pd.read_csv('classifications.csv')
 output_dir = 'sub_images'
 os.makedirs(output_dir, exist_ok=True)
 
+# Dicionário para contar quantas imagens foram salvas para cada categoria de Bethesda
+contadores = {}
+
 # Função para recortar e armazenar a sub-imagem
 def recortar_e_armazenar(imagem_path, nucleus_x, nucleus_y, bethesda_system, cell_id):
+    # Verificar se já salvamos 20 imagens dessa classificação
+    if contadores.get(bethesda_system, 0) >= 20:
+        return
+
     try:
         imagem = Image.open(imagem_path)
     except FileNotFoundError:
@@ -25,6 +32,9 @@ def recortar_e_armazenar(imagem_path, nucleus_x, nucleus_y, bethesda_system, cel
     os.makedirs(sub_dir, exist_ok=True)
     sub_imagem_path = os.path.join(sub_dir, f'{cell_id}.png')
     celula.save(sub_imagem_path)
+
+    # Atualizar contador
+    contadores[bethesda_system] = contadores.get(bethesda_system, 0) + 1
 
 # Iterar sobre as classificações e recortar as sub-imagens
 for index, row in classifications.iterrows():
